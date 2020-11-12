3f8735031f32e98231a2097bec943747a6d1256c
prachi <prachisoni24599@gmail.com>
2020-11-12 16:49:56 +0530

    header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { ComplaintComponent } from './views/complaint/complaint.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    ComplaintComponent
+    ComplaintComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
